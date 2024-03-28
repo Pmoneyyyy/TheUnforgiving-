5685248940a5b0f5e4dfbf1f82c41a9be9573087
Pmoneyyyy <Perryblake121@gmail.com>
2024-03-28 17:26:40 -0400

    Added fixed update

--- C Sharp things/obj/Debug/net8.0/C Sharp things.AssemblyInfo.cs
+++ C Sharp things/obj/Debug/net8.0/C Sharp things.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C Sharp things")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11e80ed87ee19a361423cbe05fa53f9ea4afa3f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f5a091950ddab0925d1a58ea9971c77d20d1d68")]
 [assembly: System.Reflection.AssemblyProductAttribute("C Sharp things")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C Sharp things")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
